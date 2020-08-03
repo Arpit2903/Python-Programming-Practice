19c985eb3c7ead4462a77059865c393974edd805
Arpit Patel <arpitkpatel@yahoo.com>
2020-08-03 19:47:16 -0400

    completed

---
+++ Dictionary.py
@@ -0,0 +1,6 @@
+#create a dictionary and take input from the user and return the meaning of the word from the dictionary
+
+print("Welcome to the Dictionary")
+word = input("Enter the word to search:")
+dictionary={"abase":" cause to feel shame", "benefit":"advantage", "callow":"young and inexperienced", "dally":"waste time", "endear":"make attractive"}
+print(dictionary[word])
